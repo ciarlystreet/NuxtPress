4d63a66f8f6b79887143845d34b06114de381732
Carlo La Pera <carlo.lapera@diginess.it>
2019-05-11 15:58:42 +0200

    added List component

---
+++ components/List.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <b-list-group v-if="links">
+      <b-list-group-item
+        v-for="(link, index) in links"
+        :key="index"
+        :href="link.href"
+        :title="link.title"
+        class="text-center"
+      >
+        {{ link.label }}
+      </b-list-group-item>
+    </b-list-group>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    links: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,11 @@
 <template>
   <section class="container-fluid">
-    <h1 class="text-center mb-3 font-secondary">Datti da fare!</h1>
+    <h1 v-if="title" class="text-center mb-3 font-secondary">{{ title }}</h1>
 
     <div class="container">
       <div class="row">
         <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
-          <b-list-group v-if="links">
-            <b-list-group-item
-              v-for="(link, index) in links"
-              :key="index"
-              :href="link.href"
-              :title="link.title"
-              class="text-center"
-            >
-              {{ link.label }}
-            </b-list-group-item>
-          </b-list-group>
+          <List :links="links" />
         </div>
       </div>
     </div>
@@ -23,22 +13,27 @@
 </template>
 
 <script>
+import List from '~/components/List.vue'
 export default {
+  components: {
+    List
+  },
   data() {
     return {
+      title: 'Datti da fare',
       links: [
         {
-          href: '/tutti-i-quiz-per-argomento/',
+          href: 'quiz/tutti-i-quiz-per-argomento/',
           title: 'Tutti i quiz per argomento',
           label: 'Tutti i quiz per argomento'
         },
         {
-          href: '/quiz-per-argomento/',
+          href: 'quiz/quiz-per-argomento/',
           title: 'Quiz per argomento',
           label: 'Quiz per argomento'
         },
         {
-          href: '/simulazione-esame/',
+          href: 'quiz/simulazione-esame/',
           title: 'Simulazione Esame',
           label: 'Simulazione Esame'
         },

04bf3efbd620b3973dce31f5d22ce37543f54fb2
Carlo La Pera <carlo.lapera@diginess.it>
2019-05-11 16:18:03 +0200

    added dinamic content

---
+++ pages/quiz/tutti-i-quiz-per-argomento/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="container-fluid">
+    <h1 v-if="title" class="text-center mb-3 font-secondary">{{ title }}</h1>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
+          <List :links="links" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import List from '~/components/List.vue'
+export default {
+  components: {
+    List
+  },
+  data() {
+    return {
+      title: 'Materie disponibili',
+      links: []
+    }
+  },
+  async asyncData({ $axios, error, params }) {
+    try {
+      const { data } = await $axios.get('/wp-api/arguments', { useCache: true })
+      // eslint-disable-next-line no-console
+      const links = []
+      data.forEach(element => {
+        const link = {
+          href: element.slug,
+          title: element.name,
+          label: element.name
+        }
+        links.push(link)
+      })
+      return {
+        links
+      }
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch event #' + params.id
+      })
+    }
+  }
+}
+</script>

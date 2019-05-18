445bb0ef2283c87efdf4077813288161eb26ff59
Carlo La Pera <carlo.lapera@diginess.it>
2019-05-18 16:54:53 +0200

    fixed links

--- pages/quiz/index.vue
+++ pages/quiz/index.vue
@@ -18,14 +18,18 @@ export default {
       title: 'Datti da fare',
       links: [
         {
+          // All question by argument
           href: 'quiz/tutti-i-quiz-per-argomento/',
           title: 'Tutti i quiz per argomento',
-          label: 'Tutti i quiz per argomento'
+          label: 'Tutti i quiz per argomento',
+          desc: 'Rispondi a tutte le domande di uno specifico argomento'
         },
         {
+          // 30 questions by argumnet
           href: 'quiz/quiz-per-argomento/',
           title: 'Quiz per argomento',
-          label: 'Quiz per argomento'
+          label: 'Quiz per argomento',
+          desc: 'Rispondi a 30 domande di uno specifico argomento'
         },
         {
           href: 'quiz/simulazione-esame/',

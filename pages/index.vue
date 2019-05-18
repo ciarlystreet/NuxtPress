b1c76704a8c5693d3f0b91e9756797714b14a216
Carlo La Pera <carlo.lapera@diginess.it>
2019-05-18 11:57:27 +0200

    fixed console error

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,27 @@
 <template>
-  <b-card
-    img-src="/images/quiz-time.png"
-    img-alt="Card image"
-    img-top
-    header="Benvenuto"
-  >
-    <b-card-text>
-      <div v-if="$store.state.login.auth">
-        Hai già effettuato l'accesso
-        <button @click="logout">
-          Logout
-        </button>
-      </div>
-      <div v-else>
-        <p>
-          Per poter accedere ai quiz occorre essere utenti registrati. Cosa
-          aspetti? Registrarsi è semplice. Imparare sarà più semplice del
-          previsto, ti daremo gli strumenti per non mollare, sarà un gioco da
-          ragazzi vedrai!
-        </p>
-        <nuxt-link to="auth/login">Accedi</nuxt-link> |
-        <nuxt-link to="/auth/registrazione">Registrati</nuxt-link>
-      </div>
-    </b-card-text>
-  </b-card>
+  <!-- [no-ssr] To skip error: The client-side rendered virtual DOM tree is not matching server-rendered content -->
+  <no-ssr>
+    <b-card header="Benvenuto">
+      <b-card-text>
+        <div v-if="$store.state.login.auth">
+          Hai già effettuato l'accesso
+          <button @click="logout">
+            Logout
+          </button>
+        </div>
+        <div v-else>
+          <p>
+            Per poter accedere ai quiz occorre essere utenti registrati. Cosa
+            aspetti? Registrarsi è semplice. Imparare sarà più semplice del
+            previsto, ti daremo gli strumenti per non mollare, sarà un gioco da
+            ragazzi vedrai!
+          </p>
+          <nuxt-link to="auth/login">Accedi</nuxt-link> |
+          <nuxt-link to="/auth/registrazione">Registrati</nuxt-link>
+        </div>
+      </b-card-text>
+    </b-card>
+  </no-ssr>
 </template>
 
 <script>

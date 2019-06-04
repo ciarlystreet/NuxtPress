7f812ddd43f38c04891ec6fd1ff9135efcb264af
Carlo La Pera <carlo.lapera@diginess.it>
2019-06-04 20:51:39 +0200

    fixed index

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,47 @@
 <template>
-  <!-- [no-ssr] To skip error: The client-side rendered virtual DOM tree is not matching server-rendered content -->
-  <no-ssr>
-    <b-card header="Benvenuto">
-      <b-card-text>
-        <div v-if="$store.state.login.auth">
-          Hai già effettuato l'accesso
-          <button class="btn btn-secondary text-white" @click="logout">
-            Logout
-          </button>
-        </div>
-        <div v-else>
-          <p>
-            Per poter accedere ai quiz occorre essere utenti registrati. Cosa
-            aspetti? Registrarsi è semplice. Imparare sarà più semplice del
-            previsto, ti daremo gli strumenti per non mollare, sarà un gioco da
-            ragazzi vedrai!
-          </p>
-          <nuxt-link to="auth/login" class="btn btn-info">Accedi</nuxt-link> |
-          <nuxt-link to="/auth/registrazione" class="btn btn-secondary">
-            Registrati
-          </nuxt-link>
-        </div>
-      </b-card-text>
-    </b-card>
-  </no-ssr>
+  <b-card v-if="show" header="Benvenuto">
+    <b-card-text>
+      <div v-if="is_user_logged_in">
+        Hai già effettuato l'accesso
+        <button class="btn btn-secondary text-white" @click="logout">
+          Logout
+        </button>
+      </div>
+      <div v-else>
+        <p>
+          Per poter accedere ai quiz occorre essere utenti registrati. Cosa
+          aspetti? Registrarsi è semplice. Imparare sarà più semplice del
+          previsto, ti daremo gli strumenti per non mollare, sarà un gioco da
+          ragazzi vedrai!
+        </p>
+        <nuxt-link to="auth/login" class="btn btn-info">Accedi</nuxt-link> |
+        <nuxt-link
+          to="/auth/registrazione"
+          class="btn btn-secondary text-white"
+        >
+          Registrati
+        </nuxt-link>
+      </div>
+    </b-card-text>
+  </b-card>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 const Cookie = process.client ? require('js-cookie') : undefined
 export default {
+  data() {
+    return {
+      show: true
+    }
+  },
+  computed: {
+    ...mapState({
+      is_user_logged_in({ login }) {
+        return login && login.auth !== null ? "'" + login.auth + "'" : false
+      }
+    })
+  },
   methods: {
     logout() {
       Cookie.remove('user_info')
